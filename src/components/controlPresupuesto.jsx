3a11f6d55e16ec94f282f6b87667cee59a2f2e5e
Celia Avilés <celiavicaviles@gmail.com>
2024-01-08 14:01:46 +0100

    Add controlPresupuesto component

---
+++ src/components/controlPresupuesto.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line react/prop-types
+export default function ControlPresupuesto({ presupuesto }) {
+  return (
+    <>
+      <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+        <div>
+          <p>Gráfica aquí</p>
+        </div>
+        <div className="contenido-presupuesto">
+          <p>
+            <span>Presupuesto: </span> {presupuesto} €
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,26 @@
 /* eslint-disable react/prop-types */
+import ControlPresupuesto from "./controlPresupuesto";
 import NuevoPresupuesto from "./nuevoPresupuesto";
 
-export default function Header({ presupuesto, setPresupuesto }) {
+export default function Header({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) {
   return (
     <>
       <header>
         <h1>Planificador de Gastos</h1>
-        <NuevoPresupuesto
-          presupuesto={presupuesto}
-          setPresupuesto={setPresupuesto}
-        />
+        {isValidPresupuesto ? (
+          <ControlPresupuesto presupuesto={presupuesto} />
+        ) : (
+          <NuevoPresupuesto
+            presupuesto={presupuesto}
+            setPresupuesto={setPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+          />
+        )}
       </header>
     </>
   );
