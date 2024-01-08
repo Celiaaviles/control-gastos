1092d14556c3b96d12317c805fd25d72e7121e4b
Celia Avilés <celiavicaviles@gmail.com>
2024-01-08 14:12:00 +0100

    Update controlPresupuesto component

--- src/components/controlPresupuesto.jsx
+++ src/components/controlPresupuesto.jsx
@@ -1,5 +1,12 @@
 // eslint-disable-next-line react/prop-types
 export default function ControlPresupuesto({ presupuesto }) {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("es-EU", {
+      style: "currency",
+      currency: "EUR",
+    });
+  };
+
   return (
     <>
       <div className="contenedor-presupuesto contenedor sombra dos-columnas">
@@ -8,7 +15,17 @@ export default function ControlPresupuesto({ presupuesto }) {
         </div>
         <div className="contenido-presupuesto">
           <p>
-            <span>Presupuesto: </span> {presupuesto} €
+            <span>Presupuesto: </span>
+            {formatearCantidad(presupuesto)}
+          </p>
+          <p>
+            <span>Disponible: </span>
+            {formatearCantidad(0)}
+          </p>
+
+          <p>
+            <span>Gastado: </span>
+            {formatearCantidad(0)}
           </p>
         </div>
       </div>
